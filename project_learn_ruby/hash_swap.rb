5aef35dd3a1055bf05635e4196c0881d1100035c
karsoumi <ranaghoul@gmail.com>
2016-12-22 20:47:44 +0530

    Code for swapping keys and values!

---
+++ project_learn_ruby/hash_swap.rb
@@ -0,0 +1,42 @@
+
+#!/bin/ruby -w
+
+=begin
+Swap the key-values in a hash.
+{key=>value} to {value=>key}
+=end
+
+
+myhash = { 'India' => 'ND',
+	   'France' => 'FRA',
+	   'Germany' => 'BE',
+	   'France' => 'PA'
+	   }
+
+
+puts myhash['France']
+puts myhash.invert #fails to recognize duplicates
+
+#This also fails to recognise duplicates
+myhash_new = {}
+myhash.each do |key, val|
+  myhash_new[val] = key
+  myhash.delete(key)
+end
+myhash = myhash_new
+puts myhash
+
+# DOES HASH RECOGNIZES DUPLICATES AT ALL???
+### No, it does not. In case of a duplicate key 
+### it gives precedence to the value 
+### which the last repetation of the key has. Example: 
+
+myhash = { 'India' => 'ND',
+           'France' => 'FRA', #This will be ignored
+	   'Germany' => 'BE',
+	   'France' => 'PA'
+	 }
+puts myhash['France'] 
+
+### Output: PA. FRA is ignored.
+
