583f2524a51d6e1f372561a4908d7cc61ca5231b
karso <ranaghoul@gmail.com>
2019-02-01 12:06:34 +0530

    Measuring KMS usage.
    Simple test for unit testing with API programming

---
+++ project_learn_python/api_program/aws/kms_usage.py
@@ -0,0 +1,33 @@
+import boto3
+
+
+def takeInput(num = None):
+    if num == None:
+        num = int(raw_input("Enter the AWS KMS Key Limit: ")) 
+    if num <= 0 or not isinstance(num, int):
+        return "Input must be positive integer greater than Zero."
+    else:
+        return num
+        
+
+def findUsage():
+    kmsClient = boto3.client('kms')
+    ####    ToDO: Catch connection exception
+    kmsResponseListKeys = kmsClient.list_keys()
+    usedKeys = len(kmsResponseListKeys['Keys'])
+    return usedKeys
+
+def compareKeys(keyLimit, keyInuse):
+    if keyLimit < keyInuse:
+        return "Something is wrong! KeyUsage can't be higher than KeyLimit!"
+    else:
+        return (keyLimit - keyInuse) * 100 / keyLimit
+
+
+if __name__ == '__main__':
+    keyLimit = takeInput()
+    if isinstance(keyLimit, str):
+        print keyLimit
+        exit(1)
+    keyInuse = findUsage()
+    print "Percentage of Available Keys is", compareKeys(keyLimit, keyInuse)
---
+++ project_learn_python/api_program/aws/test_kms_usage.py
@@ -0,0 +1,42 @@
+'''
+Count the number of keys in use.
+Compare against an input value.
+Return % usage
+'''
+
+import unittest
+import kms_usage
+
+class TestKMSUsage(unittest.TestCase):
+    
+    '''
+    Test input value. It should be positive integer greater than Zero.
+    '''
+    def test_takeInput(self):
+        self.assertEqual(kms_usage.takeInput(100), 100)
+        self.assertEqual(kms_usage.takeInput(0),"Input must be positive integer greater than Zero.")
+        self.assertEqual(kms_usage.takeInput(3.5),"Input must be positive integer greater than Zero.")
+        self.assertEqual(kms_usage.takeInput(-5),"Input must be positive integer greater than Zero.")
+    
+    
+    
+    '''
+    Test connection to AWS API. Test the count.
+    '''
+    def test_findUsage(self):
+        self.assertIsInstance(kms_usage.findUsage(), int)
+    
+    '''
+    Test comparison module.
+    '''
+    def test_compareKeys(self):
+        self.assertEqual(kms_usage.compareKeys(10, 3), 70)
+        self.assertEqual(kms_usage.compareKeys(10, 0), 100)
+        self.assertEqual(kms_usage.compareKeys(10, 10), 0)
+        self.assertEqual(kms_usage.compareKeys(10, 12), "Something is wrong! KeyUsage can't be higher than KeyLimit!")
+    
+    
+    
+    
+if __name__ == '__main__':
+    unittest.main()    
